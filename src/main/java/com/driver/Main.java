6e41ea1dd40c4aa199ed1fe65f354890a6f44b24
Aniket Manjare <aniketmanjare8712@gmail.com>
2024-12-10 12:06:36 +0530

    NEW2 Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,14 @@
 package com.driver;
 class A{
     public String meth(){
-      return( "Invoking method from class A") ;
+      return "Invoking method from class A";
     }
 }
 class B extends A{
 
 
   public String meth(){
-        return("Method is overridden in Extended class B") ;
+        return "Method is overridden in Extended class B";
     }
 }
 public class Main {
