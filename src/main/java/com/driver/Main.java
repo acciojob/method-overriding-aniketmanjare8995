3246981b13225d9e8c9ba8eb66c94c17f41b56ce
Aniket Manjare <aniketmanjare8712@gmail.com>
2024-12-22 01:18:01 +0530

    latest

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,25 @@
 package com.driver;
-class A{
-    public String meth(){
-      return "Invoking method from classA";
-    }
-}
-class B extends A{
 
-
-  public String meth(){
-        return "Method is overridden in Extended classB";
-    }
-}
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
     public static void main(String[] args){
         B obj=new B();
-        System.out.println(obj.meth());     //Invoking method from class A when we dont override
-                                    //method is overridden in Extended class B when we override the method
+        System.out.println(obj.meth());
 
+        B obj1=new B();
+        obj1.meth();
     }
 
   
